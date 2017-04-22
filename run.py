d014a0e42a7c74ba93e662141647fc1c90ce384a
Alexander Wåland <alexander@width.se>
2017-04-22 07:29:53 +0200

    Read app debug from os.environ

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from HittaSkyddsrum import app
+import os
 
 # We only need this for local development.
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=os.environ.get('DEBUG'))
