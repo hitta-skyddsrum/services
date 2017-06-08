de020beb7f166cab57e92c1d0fed10819af0df30
Alexander Wåland <alexander@width.se>
2017-06-08 15:41:21 +0200

    Fix CORS and encoding

--- HittaSkyddsrum/__init__.py
+++ HittaSkyddsrum/__init__.py
@@ -3,22 +3,23 @@ import os
 from flask import Flask, jsonify
 from HittaSkyddsrum.shelters.controllers import mod_shelters
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqldb://" + \
-                                        os.environ.get("MYSQL_DATABASE_USER") + ":" + \
-                                        os.environ.get("MYSQL_DATABASE_PASSWORD") + "@" + \
-                                        os.environ.get("MYSQL_DATABASE_HOST") + "/" + \
-                                        os.environ.get("MYSQL_DATABASE_DB")
-
-db = SQLAlchemy(app)
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 app.config['MYSQL_DATABASE_USER'] = os.environ.get("MYSQL_DATABASE_USER")
 app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get("MYSQL_DATABASE_PASSWORD")
 app.config['MYSQL_DATABASE_DB'] = os.environ.get("MYSQL_DATABASE_DB")
 app.config['MYSQL_DATABASE_HOST'] = os.environ.get("MYSQL_DATABASE_HOST")
 app.config['APPLICATION_ROOT'] = '/api/v1'
-app.config['JSON_AS_ASCII'] = False
+app.config['JSON_AS_ASCII'] = True
+
+CORS(app)
+
+DB_URL = 'mysql+mysqldb://{}:{}@{}/{}'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL.format(app.config['MYSQL_DATABASE_USER'], app.config['MYSQL_DATABASE_PASSWORD'], app.config['MYSQL_DATABASE_HOST'], app.config['MYSQL_DATABASE_DB'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 app.register_blueprint(mod_shelters, url_prefix='/api/v1/shelters')
 
