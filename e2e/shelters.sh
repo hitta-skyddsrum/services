d1059850ad5a19b9a8bc2237857efd26791fd98e
kontrollanten <alexander@width.se>
2018-02-09 16:30:32 +0000

    shelters: add new interface for v2

--- e2e/shelters.sh
+++ e2e/shelters.sh
@@ -46,6 +46,33 @@ response=$(curl -s "${shelters_url}?lat=59.3618&long=18.1205")
 assert_equals "$(get_json_array_length "$response")" "10"
 assert_equals "$(get_json_value "$response" ".[0].id")" "62682"
 
+echo "### END /api/v1/shelters"
+echo ""
+echo "### START /api/v2/shelters"
+shelters_url="localhost:5000/api/v2/shelters/" 
+
+echo "When retrieving a shelter accurate properties should be given"
+response=$(curl -s "${shelters_url}142784-8")
+assert_equals "$(get_json_value "$response" ".id")" "1"
+assert_equals "$(get_json_value "$response" ".shelterId")" "142784-8"
+assert_equals "$(get_json_value "$response" ".goid")" "5559A55A6966B0002070D6A1B801F496"
+assert_equals "$(get_json_value "$response" ".address")" "Sockerbruksgatan 3"
+assert_equals "$(get_json_value "$response" ".slots")" "80"
+
+echo "When passing lat and long query params, nearby shelters should be given"
+response=$(curl -s "${shelters_url}?lat=59.3618&long=18.1205")
+assert_equals "$(get_json_array_length "$response")" "10"
+assert_equals "$(get_json_value "$response" ".[0].id")" "62682"
+
+echo "When searching for hospitals an array with results should be returned"
+response=$(curl -s "${shelters_url}142784-8/hospitals")
+assert_equals "$(get_json_value "$response" ".[0].hsaId")" "SE2321000131-E000000003019"
+
+echo "### END /api/v1/shelters"
+echo ""
+echo "### START /api/v2/shelters"
+shelters_url="localhost:5000/api/v2/shelters/" 
+
 
 if [ "$Errors" -gt "0" ]; then
   exit 1
