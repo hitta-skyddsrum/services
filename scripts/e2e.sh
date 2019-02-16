beea670e39c5769d976b62fec8d4895bf195d820
kontrollanten <alexander@width.se>
2019-02-16 21:01:08 +0100

    e2e: fix wait for webserver

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
 DIR=$(dirname "$(readlink -f "$0")")
 
 python $DIR/../run.py &
-sh $DIR/wait-for-webserver.sh localhost:5000
+server_pid=$!
+sh $DIR/wait-for-webserver.sh localhost:5000 &
+waiter_pid=$!
+wait $waiter_pid || { kill -9 $server_pid; exit 1; }
 sh $DIR/../e2e/shelters.sh
--- scripts/wait-for-webserver.sh
+++ scripts/wait-for-webserver.sh
@@ -7,9 +7,9 @@ shift
 cmd="$@"
 
 until [ $(curl -s -o /dev/null -w "%{http_code}" $host) -ge "404" ]; do
-    >&2 echo "Web server  is unavailable - sleeping"
+    >&2 echo "Web server is unavailable - sleeping"
       sleep 1
     done
 
-    >&2 echo "Web server is up - executing command"
-    exec $cmd
+    >&2 echo "Web server is up"
+    exit 0
