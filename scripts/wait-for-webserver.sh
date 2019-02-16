4d1e9c8cd5697a65809a167728ff522e8b86f10a
kontrollanten <alexander@width.se>
2019-02-16 21:11:00 +0100

    wait-for-webserver: add timeout

--- scripts/wait-for-webserver.sh
+++ scripts/wait-for-webserver.sh
@@ -6,9 +6,16 @@ host="$1"
 shift
 cmd="$@"
 
+tries=0
+
 until [ $(curl -s -o /dev/null -w "%{http_code}" $host) -ge "404" ]; do
-    >&2 echo "Web server is unavailable - sleeping"
-      sleep 1
+    tries=$((tries+1))
+    >&2 echo "Web server is unavailable - sleeping for $tries seconds."
+      sleep $tries
+      if [ "$tries" -eq 10 ];then
+        echo "Failed after $tries tries."
+        exit 1
+      fi
     done
 
     >&2 echo "Web server is up"
